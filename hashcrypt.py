1b81a2686f0aac7fdde8e366731dc496dda24446
alekszdev <raquelrodriguezllano46@gmail.com>
2024-10-02 19:05:55 +0200

    Update hashcrypt.py

--- hashcrypt.py
+++ hashcrypt.py
@@ -3,158 +3,185 @@ import hashlib
 import os
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("clear || cls")
 
 
 
 def deshash_salt():
-    bruteforce_file = input("Wordlist name: ")
-    hash_input = input("Enter a Hash: ")
-    salt = input("Enter a Salt: ")
+    try:
+        bruteforce_file = input("Wordlist name: ")
+        hash_input = input("Enter a Hash: ")
+        salt = input("Enter a Salt: ")
 
-    with open(f'{bruteforce_file}', 'r', encoding='latin-1') as wordlist:
-        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Wordlist loaded")
+        with open(f'{bruteforce_file}', 'r', encoding='latin-1') as wordlist:
+            print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Wordlist loaded")
 
-        if len(hash_input) == 32:
-            type = "MD5"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+            if len(hash_input) == 32:
+                type = "MD5"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.md5((word + salt).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.md5((word + salt).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 40:
-            type = "SHA-1"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 40:
+                type = "SHA-1"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha1((word + salt).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha1((word + salt).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 64:
-            type = "SHA-256"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 64:
+                type = "SHA-256"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha256((word + salt).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha256((word + salt).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 128:
-            type = "SHA-512"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 128:
+                type = "SHA-512"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha512((word + salt).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha512((word + salt).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        else:
-            print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} Type of hash unknown")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            else:
+                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} Type of hash unknown")
+    except FileNotFoundError as e:
+        print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} An error has ocurred: {e}")
 
 
 
 def deshash_without_salt():
-    bruteforce_file = input("Wordlist name: ")
-    hash_input = input("Enter a hash: ")
-    with open(f'{bruteforce_file}', 'r', encoding='latin-1') as wordlist:
-        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Wordlist loaded")
-        if len(hash_input) == 32:
-            type = "MD5"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+    try:
+        bruteforce_file = input("Wordlist name: ")
+        hash_input = input("Enter a hash: ")
+        with open(f'{bruteforce_file}', 'r', encoding='latin-1') as wordlist:
+            print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} Wordlist loaded")
+            if len(hash_input) == 32:
+                type = "MD5"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.md5((word).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.md5((word).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 40:
-            type = "SHA-1"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 40:
+                type = "SHA-1"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha1((word).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha1((word).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 64:
-            type = "SHA-256"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 64:
+                type = "SHA-256"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha256((word).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha256((word).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        elif len(hash_input) == 128:
-            type = "SHA-512"
-            found = False
-            print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            elif len(hash_input) == 128:
+                type = "SHA-512"
+                found = False
+                print(f"{Fore.YELLOW}[{Fore.RESET}+{Fore.YELLOW}]{Fore.RESET} Type of hash: {type}")
 
-            for word in wordlist:
-                word = word.strip()
-                hash_attempt = hashlib.sha512((word).encode()).hexdigest()
+                for word in wordlist:
+                    word = word.strip()
+                    hash_attempt = hashlib.sha512((word).encode()).hexdigest()
 
-                if hash_attempt == hash_input:
-                    print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
-                    found = True
-                    
-                    break
-            if not found:
-                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
-        else:
-            print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} Type of hash unknown")
+                    if hash_attempt == hash_input:
+                        print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}]{Fore.RESET} The password has been decrypted: {word}")
+                        found = True
+                        
+                        break
+                if not found:
+                    print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} The password cant be decrypted, try with other wordlist")
+            else:
+                print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} Type of hash unknown")
+    except FileNotFoundError as e:
+        print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} An error has ocurred: {e}")
+
+
+
+def select_option(option):
+    actions = {
+        1: deshash_salt,
+        2: deshash_without_salt
+    }
+    if option in actions:
+        actions[option]()
+    else:
+        print(f"{Fore.RED}[{Fore.RESET}+{Fore.RED}]{Fore.RESET} Please enter a valid option")
+
+
+
+def try_again():
+    option = input("Try again (y/n): ")
+    if option=='y':
+        main_menu()
+
+
 
 def main_menu():
     clear_screen()
@@ -166,13 +193,10 @@ def main_menu():
 ██╔══██║██╔══██║╚════██║██╔══██║██║     ██╔══██╗  ╚██╔╝  ██╔═══╝    ██║   
 ██║  ██║██║  ██║███████║██║  ██║╚██████╗██║  ██║   ██║   ██║        ██║   
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝        ╚═╝   
-by alekszdev.
 {Fore.RESET}""")
     print(f"{Fore.CYAN}[{Fore.RESET}1{Fore.CYAN}]{Fore.RESET} HASH AND SALT.")
     print(f"{Fore.CYAN}[{Fore.RESET}2{Fore.CYAN}]{Fore.RESET} ONLY HASH.")
     option_1 = int(input("> "))
-    if option_1==1:
-        deshash_salt()
-    elif option_1==2:
-        deshash_without_salt()
+    select_option(option_1)
+    try_again()
 main_menu()
